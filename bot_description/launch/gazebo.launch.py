e886749e5ea2c2d0ca89cdc915237187e987cc28
subash <subashchandrabose2003@gmail.com>
2026-03-09 11:59:08 +0530

    Refactor launch and URDF files for improved robot simulation setup

--- bot_description/launch/gazebo.launch.py
+++ bot_description/launch/gazebo.launch.py
@@ -5,21 +5,16 @@ from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 import xacro
-from os.path import join
 
 def generate_launch_description():
 
-    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
-    pkg_ros_gz_rbot = get_package_share_directory('bot_description')
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    pkg_bot_description = get_package_share_directory('bot_description')
 
-
-    robot_description_file = os.path.join(pkg_ros_gz_rbot, 'urdf', 'bot.xacro')
-    ros_gz_bridge_config = os.path.join(pkg_ros_gz_rbot, 'config', 'ros_gz_bridge_gazebo.yaml')
-    
+    robot_description_file = os.path.join(pkg_bot_description, 'urdf', 'bot.xacro')
     robot_description_config = xacro.process_file(robot_description_file)
     robot_description = {'robot_description': robot_description_config.toxml()}
 
-   
     robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -28,40 +23,54 @@ def generate_launch_description():
         parameters=[robot_description],
     )
 
-   
     gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(join(pkg_ros_gz_sim, "launch", "gz_sim.launch.py")),
-        launch_arguments={"gz_args": "-r -v 4 empty.sdf"}.items()
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py')
+        ),
+        launch_arguments={'world': ''}.items()
     )
 
     spawn_robot = TimerAction(
-        period=5.0,  
+        period=5.0,
         actions=[Node(
-            package='ros_gz_sim',
-            executable='create',
+            package='gazebo_ros',
+            executable='spawn_entity.py',
             arguments=[
-                "-topic", "/robot_description",
-                "-name", "bot",
-                "-allow_renaming", "false",  # prevents "_1" duplicate
-                "-x", "0.0",
-                "-y", "0.0",
-                "-z", "0.32",
-                "-Y", "0.0"
+                '-topic', '/robot_description',
+                '-entity', 'bot',
+                '-x', '0.0',
+                '-y', '0.0',
+                '-z', '0.32',
+                '-Y', '0.0',
             ],
             output='screen'
         )]
     )
 
-    ros_gz_bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        parameters=[{'config_file': ros_gz_bridge_config}],
-        output='screen'
+    joint_state_broadcaster = TimerAction(
+        period=8.0,
+        actions=[Node(
+            package='controller_manager',
+            executable='spawner',
+            arguments=['joint_broad'],
+            output='screen',
+        )]
+    )
+
+    diff_drive_controller = TimerAction(
+        period=8.0,
+        actions=[Node(
+            package='controller_manager',
+            executable='spawner',
+            arguments=['diff_cont'],
+            output='screen',
+        )]
     )
 
     return LaunchDescription([
         gazebo,
-        spawn_robot,
-        ros_gz_bridge,
         robot_state_publisher,
+        spawn_robot,
+        joint_state_broadcaster,
+        diff_drive_controller,
     ])
